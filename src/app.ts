944e0916bb566aa9ba66e3fd20f2000dcb2e122b
bibhanshu12 <mail.bibhanshu127@gmail.com>
2025-05-13 22:05:14 +0530

    fix: Correct start script path and update CORS origin URL

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 // 1) CORS + Cookies
 app.use(
   cors({
-    origin: ["http://localhost:5173","http://localhost:5174"],
+    origin: ["http://localhost:5173","http:/:5174"],
     credentials: true,
   })
 );
